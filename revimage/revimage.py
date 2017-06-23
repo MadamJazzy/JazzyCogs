319b4b3c04235ade2432e9ee5f2d21deebaaf8f3
MadamJazzy <Jleehardin@live.com>
2017-06-23 08:10:29 -0400

    Disabled Gimage Command
    Disabled Gimage command as its not working

--- revimage/revimage.py
+++ revimage/revimage.py
@@ -43,45 +43,43 @@ class revimage:
                 if hidden.contents[0].startswith('Page:'):
                     pages.append('<{}>'.format(hidden.next_sibling['href']))
                 else:
-                    image_link = hidden.a['href']
+                        image_link = hidden.a['href']
 #            message = '\n**Pages:** '
 #            message += '\n**Pages:** '.join(pages)
             if image_link is not None:
                 message = '\n**Image Found:** \n<{}>'.format(image_link)
-            else:
-                message = '\n**Image not Found'
             await self.bot.reply(message)
 
-    @commands.command(pass_context=True)
-    async def gimage(self, ctx, link=None):
-        """
-        Reverse image search using google
-        usage:  .gimage <image-link> or
-                .gimage on image upload comment
-        """
-        file = ctx.message.attachments
-        if link is None and not file:
-            await self.bot.say('Message didn\'t contain Image')
-        else:
-            await self.bot.type()
-            if file:
-                url = file[0]['proxy_url']
-            else:
-                url = link
-            async with self.google_session.get('https://www.google.com/searchbyimage?image_url={}'.format(url)) as response:
-                soup = BeautifulSoup(await response.text(), 'html.parser')
-                pages = []
-                image_link = None
-                hidden = soup.find(class_='match').select('.hidden-xs')[0]
-                if hidden.contents[0].startswith('Page:'):
-                    pages.append('<{}>'.format(hidden.next_sibling['href']))
-                else:
-                    image_link = hidden.a['href']
-            message = '\n**Pages:** '
-            message += '\n**Pages:** '.join(pages)
-            if image_link is not None:
-                message += '\n**direct image:** <{}>'.format(image_link)
-            await self.bot.reply(message)
+#    @commands.command(pass_context=True)
+#    async def gimage(self, ctx, link=None):
+#        """
+#        Reverse image search using google
+#        usage:  .gimage <image-link> or
+#                .gimage on image upload comment
+#        """
+#        file = ctx.message.attachments
+#        if link is None and not file:
+#            await self.bot.say('Message didn\'t contain Image')
+#        else:
+#            await self.bot.type()
+#            if file:
+#                url = file[0]['proxy_url']
+#            else:
+#                url = link
+#            async with self.google_session.get('https://www.google.com/searchbyimage?image_url={}'.format(url)) as response:
+#                soup = BeautifulSoup(await response.text(), 'html.parser')
+#                pages = []
+#                image_link = None
+#                hidden = soup.find(class_='match').select('.hidden-xs')[0]
+#                if hidden.contents[0].startswith('Page:'):
+#                    pages.append('<{}>'.format(hidden.next_sibling['href']))
+#                else:
+#                    image_link = hidden.a['href']
+#            message = '\n**Pages:** '
+#            message += '\n**Pages:** '.join(pages)
+#            if image_link is not None:
+#                message += '\n**direct image:** <{}>'.format(image_link)
+#            await self.bot.reply(message)
 
 
 def setup(bot):
