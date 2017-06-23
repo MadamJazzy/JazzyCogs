9818b43d445f69ad4b8e60da9df357ddb3dd18ee
MadamJazzy <Jleehardin@live.com>
2017-06-23 08:40:49 -0400

    update match

--- revimage/revimage.py
+++ revimage/revimage.py
@@ -39,11 +39,12 @@ class revimage:
                 soup = BeautifulSoup(await response.text(), 'html.parser')
                 pages = []
                 image_link = None
-                hidden = soup.find(class_='match').select('.hidden-xs')[0]
-                if hidden.contents[0].startswith('Page:'):
-                    pages.append('<{}>'.format(hidden.next_sibling['href']))
-                else:
-                    image_link = hidden.a['href']
+                try:
+                    hidden = soup.find(class_='match').select('.hidden-xs')[0]
+                    if hidden.contents[0].startswith('Page:'):
+                        pages.append('<{}>'.format(hidden.next_sibling['href']))
+                    else:
+                        image_link = hidden.a['href']
                 except AttributeError:
                     message = '\n**No matches found**'
 #            message = '\n**Pages:** '
