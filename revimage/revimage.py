6767f9ad3704ee3c0658962d73affc863c73fd1b
MadamJazzy <Jleehardin@live.com>
2017-06-23 06:44:46 -0400

    commit
    commit

--- revimage/revimage.py
+++ revimage/revimage.py
@@ -20,38 +20,6 @@ class revimage:
         return tag.replace(' ', ', ').replace('_', ' ').title()
 
     @commands.command(pass_context=True)
-    async def sauce(self, ctx, link=None, similarity=80):
-        """
-        Reverse image search using saucenao
-        usage:   [p]sauce <image-link> <similarity (in percent)> or
-        [p]sauce on image upload comment <similarity (in percent)>
-        """
-        file = ctx.message.attachments
-        if link is None and not file:
-            await self.bot.say('Message didn\'t contain Image')
-        else:
-            await self.bot.type()
-            if file:
-                url = file[0]['proxy_url']
-                similarity = link
-            else:
-                url = link
-            async with self.sauce_session.get('http://saucenao.com/search.php?url={}'.format(url)) as response:
-                source = None
-                if response.status == 200:
-                    soup = BeautifulSoup(await response.text(), 'html.parser')
-                    for result in soup.select('.resulttablecontent'):
-                        if float(similarity) > float(result.select('.resultsimilarityinfo')[0].contents[0][:-1]):
-                            break
-                        else:
-                            if result.select('a'):
-                                source = result.select('a')[0]['href']
-                                await self.bot.reply('<{}>'.format(source))
-                                return
-                    if source is None:
-                        await self.bot.reply('No source over the similarity threshold')
-
-    @commands.command(pass_context=True)
     async def tineye(self, ctx, link=None):
         """
         Reverse image search using tineye
@@ -76,10 +44,12 @@ class revimage:
                     pages.append('<{}>'.format(hidden.next_sibling['href']))
                 else:
                     image_link = hidden.a['href']
-            message = '\n**Pages:** '
-            message += '\n**Pages:** '.join(pages)
+#            message = '\n**Pages:** '
+#            message += '\n**Pages:** '.join(pages)
             if image_link is not None:
-                message += '\n**direct image:** <{}>'.format(image_link)
+                message += '\n**Image Found:** <{}>'.format(image_link)
+            else:
+                message += '\n**Image not Found'
             await self.bot.reply(message)
 
     @commands.command(pass_context=True)
