7f29e7403094f9fcca5dccfd07c97f23c8a94add
MadamJazzy <MadamJazzy@users.noreply.github.com>
2018-08-18 02:03:45 -0400

    4

--- bancheck/bancheck.py
+++ bancheck/bancheck.py
@@ -48,7 +48,7 @@ class BanList():
         head = {'Authorization': 'token {}'.format(myToken)}
         params = {"user": user}
         resp = requests.get(equrl, headers=head, params=params)
-        final = await resp.json()
+        final = resp.json()
         return final
 
 
@@ -106,11 +106,10 @@ class BanList():
         #Equalizer Bot lookup
         try:
             final = self.eqlookup(user)
-            data = json.loads(final)
-            userid = data["id"]
-            name = data["name"] + data["discriminator"]
-            reason = data["reason"]
-            proof = self.cleanurl(data["proof"])
+            userid = final["id"]
+            name = final["name"] + final["discriminator"]
+            reason = final["reason"]
+            proof = self.cleanurl(final["proof"])
             niceurl = "[Click Here]({})".format(proof)
             description = (
                 """**Name:** {}\n**ID:** {}\n**Reason:** {}\n**Proof:** {}""".format(
